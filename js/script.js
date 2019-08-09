d5e4932617185bed9ab1d86ea0917f76bdda0ea0
Arya Wicaksana <wicaksana94@gmail.com>
2019-08-09 23:55:18 +0700

    empty search result

--- js/script.js
+++ js/script.js
@@ -129,12 +129,14 @@ $('#search-result').on('click', '.see-detail', function() {
 $('.navbar-brand').on('click', function() {
     show_loading();
     $('#main-content').html('');
+    $('#search-result').html('');
     $('#main-content').load('home.html');
 });
 
 $('.nav-page-search').on('click', function() {
     show_loading();
     $('#main-content').html('');
+    $('#search-result').html('');
     $('#main-content').load('search.html');
 });
 
